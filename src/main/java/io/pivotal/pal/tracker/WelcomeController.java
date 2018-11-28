7bd06fb9f616189da0edb858da02bdf5f271d296
Avinash023 <Avinash_Kumar_Gumpar@Dell.com>
2018-11-28 15:14:56 -0700

    First lab files - commit2

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -2,13 +2,16 @@ package io.pivotal.pal.tracker;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.beans.factory.annotation.Value;
 
 @RestController
 public class WelcomeController {
 
-    @Value("${WELCOME_MESSAGE}")
-    private ;String helloMsg = "";
+    private String helloMsg = "";
+
+
+    public WelcomeController(@Value("${WELCOME_MESSAGE}") String message) {
+        this.helloMsg = message;
+    }
 
     @GetMapping("/")
     public String sayHello() {
