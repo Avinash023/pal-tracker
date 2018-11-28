bbf8fc0e161511a177544a4f159fff15299e4da9
Avinash023 <Avinash_Kumar_Gumpar@Dell.com>
2018-11-28 15:52:56 -0700

    Deploy a Spring Boot app to Cloud Foundry - Lab Files

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -2,6 +2,7 @@ package io.pivotal.pal.tracker;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
 
 @RestController
 public class WelcomeController {
